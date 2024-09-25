9595a97961984dd0b883f8609bb91cee8e71b635
fra. <168530113+fr4xx@users.noreply.github.com>
2024-09-25 12:02:11 +0200

    Added GUI and chat functionality

--- src/com/fStudio/javachatapp/ChatClient.java
+++ src/com/fStudio/javachatapp/ChatClient.java
@@ -5,46 +5,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.util.function.Consumer;
 
 public class ChatClient {
+	private Socket socket;
+	private BufferedReader in;
+	private PrintWriter out;
+	private Consumer<String> onMessageReceived;
 
-	// Attributes
-	private Socket socket = null;
-	private BufferedReader inputConsole = null;
-	private PrintWriter out = null;
-	private BufferedReader in = null;
+	public ChatClient(String serverAddress, int serverPort, Consumer<String> onMessageReceived)
+			throws IOException {
+		this.socket = new Socket(serverAddress, serverPort);
+		this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		this.out = new PrintWriter(socket.getOutputStream(), true);
+		this.onMessageReceived = onMessageReceived;
+	}
 
-	// Constructors
-	public ChatClient(String address, int port) throws IOException {
-		try {
-			socket = new Socket(address, port);
-			System.out.println("Connected to the chat server!");
+	public void sendMessage(String msg) {
+		out.println(msg);
+	}
 
-			inputConsole = new BufferedReader(new InputStreamReader(System.in));
-			out = new PrintWriter(socket.getOutputStream(), true);
-			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-			String line = "";
-			while (!line.equals("exit")) {
-				line = inputConsole.readLine();
-				out.println(line);
-				System.out.println(in.readLine());
+	public void startClient() {
+		new Thread(() -> {
+			try {
+				String line;
+				while ((line = in.readLine()) != null) {
+					onMessageReceived.accept(line);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-
-			socket.close();
-			inputConsole.close();
-			out.close();
-		} catch (UnknownHostException u) {
-			System.out.println("Host unknown: " + u.getMessage());
-		} catch (IOException i) {
-			System.out.println("Unexpected exception: " + i.getMessage());
-		}
+		}).start();
 	}
-
-	// Methods
-	public static void main(String args[]) throws IOException {
-		ChatClient client = new ChatClient("127.0.0.1", 5000);
-	}
-
 }
